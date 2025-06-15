dd6781b8c4de390d3e668e71f091f3006a5301d6
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-15 13:12:41 +0700

    feat(utils): add GoBack function to wait for user confirmation before returning

--- utils/clear.go
+++ utils/clear.go
@@ -1,24 +1,29 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"os/exec"
 	"runtime"
 )
 
-func Clear(){
+func Clear() {
 	switch runtime.GOOS {
-    case "linux", "darwin":
-        cmd := exec.Command("clear")
-        cmd.Stdout = os.Stdout
-        cmd.Run()
-    case "windows":
-        cmd := exec.Command("cmd", "/c", "cls")
-        cmd.Stdout = os.Stdout
-        cmd.Run()
-    default:
-        fmt.Println("\n\n\n")
-    }
+	case "linux", "darwin":
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		fmt.Println("\n\n\n")
+	}
 }
 
+func Pause(reader *bufio.Reader) {
+	fmt.Print("\nPress Enter to back...")
+	reader.ReadString('\n')
+}

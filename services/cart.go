efa473a2e6e3246961e792d101bdf22f502f0041
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-22 22:47:26 +0700

    refactor: use goroutine in Cart and Checkout, extract GetCart to cart.go

--- services/cart.go
+++ services/cart.go
@@ -6,44 +6,72 @@ import (
 	"go-booking-menu/utils"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 func Cart() {
+	var wg sync.WaitGroup
+	checkoutChan := make(chan bool)
+	deleteChan := make(chan string)
+
 	for {
 		utils.Clear()
 		menus.CafeName()
-		if len(menus.Orders) == 0 {
-			fmt.Println("\nYour Cart is Empty:")
-			time.Sleep(time.Second)
-			return
-		}
-		fmt.Println("\nYour Cart:")
-		var total int
-		for i, item := range menus.Orders {
-			total += item.Price * item.Quantity
-			fmt.Printf("%d. %s\n", i+1, item.Display())
-		}
-		fmt.Printf("\nTotal: %s\n", menus.FormatRupiah(total))
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			GetCart()
+		}()
+		wg.Wait()
+
 		fmt.Printf("\n[d] Delete Item | [c] Checkout | [b] Back Menu\n")
 		fmt.Print("\nChoose option : ")
 		input := utils.Input()
 		switch strings.ToLower(input) {
 		case "c":
-			Checkout()
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				Checkout()
+				checkoutChan <- true
+			}()
+			<-checkoutChan
+			return
 		case "b":
 			return
 		case "d":
 			fmt.Print("Choose No Item Product : ")
-			input := utils.Input()
-			DeleteProductCart(input)
+			deleteInput := utils.Input()
+			wg.Add(1)
+			go func(input string) {
+				DeleteProductCart(input, &wg)
+				deleteChan <- input
+			}(deleteInput)
+			<-deleteChan
 		default:
 			utils.InvalidInput()
 		}
 	}
 }
 
-func DeleteProductCart(input string) {
+func GetCart() {
+	if len(menus.Orders) == 0 {
+		fmt.Println("\nYour Cart is Empty.")
+		time.Sleep(time.Second)
+		MainMenu()
+	}
+	fmt.Println("\nYour Cart:")
+	var total int
+	for i, item := range menus.Orders {
+		total += item.Price * item.Quantity
+		fmt.Printf("%d. %s\n", i+1, item.Display())
+	}
+	fmt.Printf("\nTotal: %s\n", menus.FormatRupiah(total))
+}
+
+func DeleteProductCart(input string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	index, err := strconv.Atoi(strings.TrimSpace(input))
 	if err != nil || index < 1 || index > len(menus.Orders) {
 		fmt.Println("\nInvalid item number.")
@@ -51,15 +79,8 @@ func DeleteProductCart(input string) {
 		return
 	}
 
-	done := make(chan bool)
-
-	go func() {
-		index--
-		menus.Orders = append(menus.Orders[:index], menus.Orders[index+1:]...)
-		done <- true
-	}()
-
-	<-done
+	index--
+	menus.Orders = append(menus.Orders[:index], menus.Orders[index+1:]...)
 
 	fmt.Println("\nItem has been removed from your cart.")
 	time.Sleep(time.Second)
--- services/checkout.go
+++ services/checkout.go
@@ -3,18 +3,35 @@ package services
 import (
 	"fmt"
 	"go-booking-menu/menus"
+	"go-booking-menu/utils"
 	"os"
+	"sync"
+	"time"
 )
 
 func Checkout() {
+	utils.Clear()
 	menus.CafeName()
-	fmt.Println("\nYour Order:")
-	var total int
-	for i, item := range menus.Orders {
-		total += item.Price * item.Quantity
-		fmt.Printf("%d. %s\n", i+1, item.Display())
-	}
-	fmt.Printf("\nTotal: %s\n", menus.FormatRupiah(total))
-	fmt.Println("\nThanks for choosing Temu Deka!")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		GetCart()
+	}()
+	wg.Wait()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println("\nProcessing payment...")
+		time.Sleep(2 * time.Second)
+		fmt.Println("\nPayment completed!")
+	}()
+
+	wg.Wait()
+
+	fmt.Printf("\nThanks for choosing Temu Deka!")
+	time.Sleep(2 * time.Second)
 	os.Exit(0)
 }

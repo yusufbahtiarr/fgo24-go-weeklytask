78015236fd87ed5e06fb19f70add45750290596b
yusufbahtiarr <yusufbdev@gmail.com>
2025-06-22 16:11:16 +0700

    feat(cart): add delete item from cart feature

--- services/cart.go
+++ services/cart.go
@@ -1,25 +1,61 @@
 package services
 
 import (
-	"bufio"
 	"fmt"
 	"go-booking-menu/menus"
 	"go-booking-menu/utils"
-	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func Cart() {
-	reader := bufio.NewReader(os.Stdin)
-	menus.CafeName()
-	fmt.Println("\nYour Order:")
-	var total int
-	for i, item := range menus.Orders {
-		total += item.Price * item.Quantity
-		fmt.Printf("%d. %s\n", i+1, item.Display())
+	for {
+		utils.Clear()
+		menus.CafeName()
+		fmt.Println("\nYour Order:")
+		var total int
+		for i, item := range menus.Orders {
+			total += item.Price * item.Quantity
+			fmt.Printf("%d. %s\n", i+1, item.Display())
+		}
+		fmt.Printf("\nTotal: %s\n", menus.FormatRupiah(total))
+		fmt.Printf("\n[d] Delete Item | [c] Checkout | [b] Back Menu\n")
+		fmt.Print("\nChoose option : ")
+		input := utils.Input()
+		switch strings.ToLower(input) {
+		case "c":
+			Checkout()
+		case "b":
+			return
+		case "d":
+			fmt.Print("Choose No Item Product : ")
+			input := utils.Input()
+			DeleteProductCart(input)
+		default:
+			utils.InvalidInput()
+		}
 	}
-	fmt.Printf("\nTotal: %s\n", menus.FormatRupiah(total))
-	fmt.Print("\nPress Enter to go back.. ")
-	reader.ReadString('\n')
-	utils.Clear()
-	MainMenu()
+}
+
+func DeleteProductCart(input string) {
+	index, err := strconv.Atoi(strings.TrimSpace(input))
+	if err != nil || index < 1 || index > len(menus.Orders) {
+		fmt.Println("\nInvalid item number.")
+		time.Sleep(time.Second)
+		return
+	}
+
+	done := make(chan bool)
+
+	go func() {
+		index--
+		menus.Orders = append(menus.Orders[:index], menus.Orders[index+1:]...)
+		done <- true
+	}()
+
+	<-done
+
+	fmt.Println("\nItem has been removed from your cart.")
+	time.Sleep(time.Second)
 }
